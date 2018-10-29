02a281debccfd30e054e511c853ac6463d18f88b
I322871 <cong.wang@sap.com>
2018-10-29 12:36:11 +0800

    User could specify their url now.

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({time: 120});
+    chrome.storage.sync.set({time: 120, scrumUrls: ['sapjira.wdf.sap.corp']});
     
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -10,5 +10,13 @@ chrome.runtime.onInstalled.addListener(function() {
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    chrome.storage.sync.get({scrumUrls: []}, function(data) {
+        if (data.scrumUrls.filter(url => tab.url.includes(url)).length){
+            chrome.tabs.executeScript(tabId, {file: 'myScript.js'});
+        }
     });
 });
--- options.js
+++ options.js
@@ -1,18 +1,55 @@
-var input = document.getElementById('optionInput');
+var timeInput = document.getElementById('timeInput');
+var urlInput = document.getElementById('urlInput');
 var saveButton = document.getElementById('saveButton');
+var inputStatus = {
+    timeInput: true,
+    urlInput: true
+}
 
-chrome.storage.sync.get({time: 120}, function(items) {
-    input.setAttribute('value', items.time);
+chrome.storage.sync.get({time: 120, scrumUrls: []}, function(items) {
+    timeInput.setAttribute('value', items.time);
+    urlInput.setAttribute('value', items.scrumUrls.join('; '));
 });
 
-input.onchange = function(e) {
+timeInput.onchange = function(e) {
     if (e.target.value <= 0) {
         saveButton.setAttribute('disabled', true);
-    } else {
+        inputStatus.timeInput = false;
+        return;
+    } else if (inputStatus.urlInput) {
         saveButton.removeAttribute('disabled');
     }
+    inputStatus.timeInput = true;
+}
+
+urlInput.onchange = function(e) {
+    if (!e.target.value.length) {
+        saveButton.setAttribute('disabled', true);
+        inputStatus.urlInput = false;
+        return;
+    } else if (inputStatus.timeInput) {
+        saveButton.removeAttribute('disabled');
+    }
+    inputStatus.urlInput = true;
 }
 
 saveButton.onclick = function() {
-    chrome.storage.sync.set({time: input.value});
+    var urls = urlInput.value.split(';');
+    for (var i in urls) {
+        urls[i] = urls[i].trim();
+    }
+    urls = urls.filter(url => url.length);
+    
+    var urlConditions = [];
+    urls.forEach(url => urlConditions.push(new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {urlContains: url},
+    })));
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: urlConditions,
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+    
+    chrome.storage.sync.set({time: timeInput.value, scrumUrls: urls});
 }
